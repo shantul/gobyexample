5e16df187a4593d88073c6b5b7b0605ce2d2c6f3
Shantul Sharma <shantul.sharma@gmail.com>
2017-05-10 11:57:43 +0100

    ~

---
+++ goroutines.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+	//synchronous execution
+
+	f("direct")
+
+	//concurrent execution
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("Done")
+}
